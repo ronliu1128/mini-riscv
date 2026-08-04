98f0140ac6c75eefbe9b543803db72d22fd605f5
ronliu1128 <ronliu20001128@gmail.com>
2026-08-05 00:20:18 +0200

    feat: add counter simulation with Verilator

--- Makefile
+++ Makefile
@@ -1,13 +1,13 @@
-# 設定模組與檔名
-TOPNAME = counter
-NXPRJ_PATH ?= $(shell pwd)
-VERILATOR_INPUT = rtl/$(TOPNAME).v sim/tb_counter.cpp
+TOP = counter
 
-default:
-	verilator --cc --exe --build -j 0 -Wall $(VERILATOR_INPUT) --top-module $(TOPNAME)
+run:
+	verilator --cc rtl/$(TOP).v \
+		--exe sim/tb_$(TOP).cpp \
+		--build \
+		--trace \
+		--top-module $(TOP)
 
-sim: default
-	./obj_dir/V$(TOPNAME)
+	./obj_dir/V$(TOP)
 
 clean:
-	rm -rf obj_dir
+	rm -rf obj_dir waveform.vcd
--- sim/tb_counter.cpp
+++ sim/tb_counter.cpp
@@ -1,11 +1,22 @@
 #include <cstdio>
 #include "verilated.h"
 #include "Vcounter.h"
+#include "verilated_vcd_c.h"   // <-- 新增
+
+vluint64_t sim_time = 0;
 
 int main(int argc, char** argv) {
     Verilated::commandArgs(argc, argv);
 
     Vcounter* top = new Vcounter;
+
+    Verilated::traceEverOn(true);
+
+    VerilatedVcdC* tf = new VerilatedVcdC;
+
+    top->trace(tf, 99);
+
+    tf->open("wave.vcd");
 
     top->rst = 1;
 
@@ -13,8 +24,14 @@ int main(int argc, char** argv) {
         top->clk = 0;
         top->eval();
 
+        tf->dump(sim_time);
+        sim_time += 5;
+
         top->clk = 1;
         top->eval();
+
+        tf->dump(sim_time);
+        sim_time += 5;
 
         if (i == 2) {
             top->rst = 0;
@@ -22,7 +39,9 @@ int main(int argc, char** argv) {
 
         printf("count = %d\n", top->count);
     }
-
+    
+    tf->close();
+    delete tf;
     delete top;
     return 0;
 }
