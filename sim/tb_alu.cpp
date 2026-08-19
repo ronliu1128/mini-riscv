7fdb3fd8d08ab2934c4af9d2bfbc17cb2c3ae728
ronliu1128 <ronliu20001128@gmail.com>
2026-08-19 23:49:18 +0200

    feat: ALU testbench with random test

--- sim/tb_alu.cpp
+++ sim/tb_alu.cpp
@@ -4,14 +4,26 @@
 #include "verilated.h"
 //#include "verilated_vcd_c.h"
 #include "Valu.h"
+#include <random>
 
-void check(
+uint32_t reference_model(uint32_t a, uint32_t b, uint8_t op)
+{
+    switch (op) {
+        case 0: return a + b;
+        case 1: return a - b;
+        case 2: return a & b;
+        case 3: return a | b;
+        case 4: return a ^ b;
+        default: return 0;
+    }
+}
+
+bool check(
     Valu *top, 
     uint32_t a,
     uint32_t b,
     uint8_t op,
-    uint32_t expected,
-    bool expected_zero
+    int patterns
 )
 {
     top->a = a;
@@ -20,11 +32,16 @@ void check(
     
     top->eval();
 
-    if (top->result!= expected || top->zero != expected_zero) {
-        printf("[FAIL] op=%u, result=%u, expected=%u, zero=%u, expected_zero=%u\n", op, top->result, expected, top->zero, expected_zero);
+    uint32_t expected = reference_model(a, b, op);
+    bool expected_zero = (expected == 0);
+
+    if (top->result!= expected|| top->zero != expected_zero) {
+        printf("[Pattern.%d FAIL] a=%u, b=%u, op=%u, result=%u, expected=%u, zero=%u, expected_zero=%u\n",patterns, a, b, op, top->result, expected, top->zero, expected_zero);
+        return false;
     }
     else {
-        printf("[PASS] op=%u, result=%u, expected=%u, zero=%u, expected_zero=%u\n", op, top->result, expected, top->zero, expected_zero);
+        //printf("[PASS] a=%u, b=%u, op=%u, result=%u, expected=%u, zero=%u, expected_zero=%u\n", a, b, op, top->result, expected, top->zero, expected_zero);
+        return true;
     }
 }
 
@@ -33,19 +50,36 @@ int main(int argc, char **argv) {
 
     Valu *top = new Valu;
 
+    std::mt19937 rng(12345);
+    
+    int patterns = 100;
+    int pass_cnt = 0;
+    int fail_cnt = 0;
+
     //Verilated::traceEverOn(true);
-
     //VerilatedVcdC* tf = new VerilatedVcdC;
-
     //top->trace(tf, 99);
-
     //tf->open("wave.vcd");   
 
-    check(top, 10, 3, 0, 13, false);
-    check(top, 10, 3, 1, 7, false);
-    check(top, 10, 3, 2, 2, false);
-    check(top, 10, 3, 3, 11, false);
-    check(top, 10, 3, 4, 9, false);
+    for (int i = 0; i < patterns; i++) {
+        uint32_t a = rng();
+        uint32_t b = rng();
+        uint8_t op = rng() % 5;
+
+        if (check(top, a, b, op, i)) {
+            pass_cnt++;
+        } else {
+            fail_cnt++;
+        }
+    }
+
+    
+    printf("=== RANDOM TEST ===\n Total Tests: %d\n Passed: %d\n Failed: %d\n", patterns, pass_cnt, fail_cnt);
+    if (fail_cnt == 0) {
+        printf("All tests passed!\n");
+    } else {
+        printf("YOU failed, HAHA!\n");
+    }
 
     delete top;
 
